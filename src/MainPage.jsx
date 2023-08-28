bcfb95e35cc4c65d2b0ba388f4402202778d850a
Aquib Mahmood <aquibmahmood106@gmail.com>
2023-08-28 19:06:54 +0530

    Added pic

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -12,12 +12,23 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const imageURL  = "https://www.logodesignteam.com/blog/wp-content/uploads/2014/04/Increase-the-Sales-Revenue-Go-for-Website-Add-Ons.jpg";
 export default function BasicGrid() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
         <Grid item xs={6}>
-          <Item style={{height : "1200px"}}>xs=6</Item>
+          <Item style={{height : "1200px"}}>
+            <div  style={{position: "absolute",
+    height:"1200px",
+    width:"900px",
+    backgroundImage: `url(${imageURL})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat"
+            }}>
+            </div>
+          </Item>
         </Grid>
         <Grid item xs={6}>
           <Item style={{height : "1200px"}}>xs=6</Item>
